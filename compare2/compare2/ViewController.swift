91701b23b8c90b1db64ce731ec26e14d68d69a59
Shuyang Yu <yu2@cooper.edu>
2016-10-07 12:46:16 -0400

    first push

---
+++ compare2/compare2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  compare2
+//
+//  Created by 于舒洋 on 10/7/16.
+//  Copyright © 2016 Compare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
