66edea3b8d0de7a905ffdb9f865f4f6ccad6f27b
Shuyang Yu <yu2@cooper.edu>
2016-10-07 20:08:40 -0400

    tab bar setup

--- compare2/compare2/AppDelegate.swift
+++ compare2/compare2/AppDelegate.swift
@@ -21,9 +21,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window =  UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = UINavigationController( rootViewController: ViewController() )
+       
         
         
-        
+
         return true
     }
 
--- compare2/compare2/ViewController.swift
+++ compare2/compare2/ViewController.swift
@@ -8,10 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController, UITabBarControllerDelegate  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationItem.title = "Chats"
+        
+        
+        // main tab bar items
+        // Create chats tab
+        let tabChats = chatsController()
+        let tabChatsBarItem = UITabBarItem(title: "Chats", image: UIImage(named: "chats.png"), selectedImage: UIImage(named: "selectedImage.png"))
+        
+        tabChats.tabBarItem = tabChatsBarItem
+        
+        
+        // Create moments tab
+        let tabMoments = momentsController()
+        let tabMomentsItem = UITabBarItem(title: "Moments", image: UIImage(named: "moments.png"), selectedImage: UIImage(named: "selectedImage2.png"))
+        tabMoments.tabBarItem = tabMomentsItem
+        
+        
+        // Create profile tab
+        let tabProfile = profilePicController()
+        let tabProfileBarItem2 = UITabBarItem(title: "Profile", image: UIImage(named: "profile.png"), selectedImage: UIImage(named: "selectedImage2.png"))
+        
+        tabProfile.tabBarItem = tabProfileBarItem2
+        
+        
+        // Create user tab
+        let tabUser = userController()
+        let tabUserItem = UITabBarItem(title: "User",  image: UIImage(named: "me.png"), selectedImage: UIImage(named: "selectedImage2"))
+        tabUser.tabBarItem = tabUserItem
+        
+        
+        self.viewControllers = [tabChats, tabMoments, tabProfile, tabUser]
+    }
+    
+    // UITabBarControllerDelegate method
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        print("Selected \(viewController.title!)")
     }
 
 
